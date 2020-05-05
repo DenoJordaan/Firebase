c275225482a375139a1a700b858c464fcfb0c186
Deno Jordaan <djordaancheat@gmail.com>
2020-05-05 09:44:47 +0200

    chapter 9 complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import * as firebase from 'firebase';
+import User from './User';
+import UserForm from './UserForm';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 class App extends Component {
-  constructor(){
-    super();
-    console.log(firebase);
-  }
-  render() {
-    return (
-        <div>
-          <h1></h1>
-        </div>
-    );
-  }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Switch>
+                            <Route path="/edit/:id" component={UserForm} />
+                            <Route path="/add" component={UserForm} />
+                            <Route exact path="/" component={User} />
+                            <Route path="/*" component={NotFound} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
+
 export default App;
+
+class NotFound extends Component {
+    render(){
+        return <div>Not Found</div>
+    }
+}
